308a72368fc11133f1de369e4e817891bd1ca28c
Ajawu David <ajawudavid@gmail.com>
2021-02-05 13:24:47 +0100

    feat: completed authentication popups

--- cgf_website/urls.py
+++ cgf_website/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('account/', include('allauth.account.urls')),
     path('', include('cgf.urls', namespace='cgf')),
 ]
 
--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', views.dummy_user_view, name='dummy_user_view'),
+    path('login/', views.dummy_user_view, name='dummy_user_view'),
+    path('register-request/', views.dummy_user_view, name='register_request'),
+    path('password-set/', views.dummy_user_view, name='dummy_user_view'),
 ]
